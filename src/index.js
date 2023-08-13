f327e7be894d35bdbf1f0a7ab2939a1257a66836
homosapien14 <spma259@gmail.com>
2023-08-13 20:05:22 +0530

    quick fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-require("dotenv").config();
 const express = require("express");
+const {PORT} = require('./config/serverConfig');
+
 
 const setupAndStartServer = () => {
   const app = express();
 
-  app.listen(process.env.PORT, () => {
-    console.log("listening on port ", process.env.PORT);
+  app.listen(PORT, () => {
+    console.log("server is up on port ", PORT);
   });
 };
 
